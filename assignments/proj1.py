cb97cba9a5ff96bc3b014625113287c3e56614cc
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2025-02-14 20:20:38 -0800

    wip on proj1

--- assignments/proj1.py
+++ assignments/proj1.py
@@ -2,34 +2,75 @@ from search import *
 
 
 class MissCannibalsVariant(Problem):
-    """The problem of Missionaries and Cannibals.
+    """
+    The problem of Missionaries and Cannibals.
     N1 and N2 are the total number of missionaries and cannibals starting from the
     left bank.
+    
     A state is represented as a 3-tuple, two numbers and a boolean:
     state[0] is the number of missionaries on the left bank (note: the number of
     missionaries on the right bank is N1-m)
+    
     state[1] is the number of cannibals on the left bank (note: the number of
     cannibals on the right bank is N2-c)
-    state[2] is true if boat is at the left bank, false if at the right bank"""
+    
+    state[2] is true if boat is at the left bank, false if at the right bank
+    
+    boat capacity is 3
+    """
 
     def __init__(self, N1=4, N2=4, goal=(0, 0, False)):
-        """Define goal state and initialize a problem"""
         initial = (N1, N2, True)
         self.N1 = N1
         self.N2 = N2
+        # assumes boat capacity is 3, so number of possible actions is 3^2 -> 9
+        self.possible_actions = ["M", "C", "MC", "MM", "CC", "MMM", "CCC", "MCM", "MCC"]
         super().__init__(initial, goal)
-    
-    def result(self, state, action):
-        m, c, b = state
 
-    def actions(self, state):
+    # return new state as result of an action
+    def result(
+        self, state: tuple[int, int, bool], action: str
+    ) -> tuple[int, int, bool]:
         m, c, b = state
+        numMiss, numCann = action.count("M"), action.count("C")
+        # add missionaries and/or cannibals if going back to left bank
+        return (
+            (m - numMiss, c - numCann, not b)
+            if b
+            else (m + numMiss, c + numCann, not b)
+        )
+
+    # Should return list of actions
+    def actions(self, state: tuple[int, int, bool]) -> list[str]:
+        (
+            m,
+            c,
+            b,
+        ) = state
+        actions = []
+        # go through all posisble actions
+        for a in self.possible_actions:
+            numMiss, numCann = a.count("M"), a.count("C")
+            if b:
+                newM, newC = m - numMiss, c - numCann
+            else:
+                newM, newC = m + numMiss, c + numCann
+            if (
+                (newM >= 0 and newC >= 0)
+                and (newM == 0 or newM >= newC)
+                and ((self.N1 - newM == 0 or self.N1 - newM >= self.N2 - newC))
+            ):
+                actions.append(a)
+        return actions
 
 
 if __name__ == "__main__":
     mc = MissCannibalsVariant(4, 4)
-    # print(mc.actions((3, 3, True))) # Test your code as you develop! This should return ['MC', 'MMM']
+    print(mc.result((2, 2, False), "M"))
+    print(
+        mc.actions((3, 3, True))
+    )  # Test your code as you develop! This should return ['MC', 'MMM']
     path = depth_first_graph_search(mc).solution()
-    print(path)
+    print("dfs", path)
     path = breadth_first_graph_search(mc).solution()
-    print(path)
+    print("bfs", path)
