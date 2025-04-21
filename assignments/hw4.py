a54c1b246097483995eaa521cf672ac948e5dc44
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2025-04-21 12:00:51 -0700

    run format

--- assignments/hw4.py
+++ assignments/hw4.py
@@ -1,5 +1,6 @@
-def q3(qw,qb,rw,rb,kw,kb,bw,bb,pw,pb):
-    return 9*(qw-qb)+5*(rw-rb)+3*(kw-kb)+3*(bw-bb)+1*(pw-pb)
+def q3(qw, qb, rw, rb, kw, kb, bw, bb, pw, pb):
+    return 9 * (qw - qb) + 5 * (rw - rb) + 3 * (kw - kb) + 3 * (bw - bb) + 1 * (pw - pb)
 
-x = q3(0,0,1,2,1,2,2,1,7,6)
+
+x = q3(0, 0, 1, 2, 1, 2, 2, 1, 7, 6)
 print("q3:", x)
